18e5dee9d85d4bf80bcc428f7a4b9821e8aa721b
jazminrh <jhinds@aoiths.org>
2016-04-09 19:08:37 +0000

    Refactor to use MVC-style routting

---
+++ controllers/users.js
@@ -0,0 +1,9 @@
+// Var controller
+
+var Users = {
+    index: function(req, res) {
+        res.sender('respond with a resource');
+    }
+};
+
+module.exports = Users;
---
+++ routes.js
@@ -0,0 +1,16 @@
+//Local Dependecies 
+var controllers = require('./controllers');
+
+var Router = {
+    
+    init: function(app) {
+        //Home page
+        app.get('/', controllers.Home.index);
+        
+        //Users
+        app.get('/users', controllers.Users.index);
+        
+    }
+};
+
+module.exports = Router;
