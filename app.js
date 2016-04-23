2a2c53f93e5600e46ddb607b4cd28205cfedf5f1
jazminrh <jhinds@aoiths.org>
2016-04-23 18:55:23 +0000

    Use nunjucks as templating engine

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+//Global dependencies 
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var consolidate = require('consolidate');
+var nunjucks = require('nunjucks');
 
+//Local dependencies 
 var router = require('./routes');
 
+//Create App instance
 var app = express();
 
 // view engine setup
+//Sets location of view directory 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//Configure nunjucks engine so it knows where views are located
+consolidate.requires.nunjucks = nunjucks.configure('views');
+//Assign nunjucks engine for .html files
+app.engine('html', consolidate.nunjucks);
+//sets html as default view engine
+app.set('view engine', 'html');
 
+//Middlewares setup
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
